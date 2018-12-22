ab90f945b67979ffc82807e8a478cc4dcb0d8d48
Agaaress <46050025+Agaaress@users.noreply.github.com>
2018-12-22 11:45:44 +0100

    Update mapa.cpp

--- rakieta/mapa.cpp
+++ rakieta/mapa.cpp
@@ -6,42 +6,125 @@
 
 using namespace std;
 
-void Mapa::generator()
+void Mapa::generator() //generowanie mapy
 {
-    for(int x=0; x<30; x++)
+    for(int y=0; y<30; y++)
    {
-       for(int y=0; y<50; y++)
+       for(int x=0; x<50; x++)
        {
-           mapa[x][y] = 255;
+           mapa[y][x] = 255;
        }
    }
 }
 
-void Mapa::wyswietlenie()
+void Mapa::wyswietlenie(int r1, int r2) //wyswietlanie mapy
 {
     system("cls");
     char border = 219;
-    for(int x=0; x<30; x++)
+    for(int y=0; y<30; y++)
     {
         cout << " " << border;
-        for(int y=0; y<50; y++)
+        for(int x=0; x<50; x++)
         {
-            mapa[0][y]=border;
-            mapa[29][y]=border;
-            cout << mapa[x][y];
+            mapa[0][x]=border;
+            mapa[29][x]=border;
+            cout << mapa[y][x];
         }
-        cout<< border << endl;
-
+        if(y==1){ cout<< border << "   X: " << r2 <<endl; }
+        else if(y==2){ cout<< border << "   Y: " << r1 <<endl; }
+        else cout<< border << endl;
     }
 }
 
-void Mapa::rakieta(int x, int y)
+void Mapa::rakieta(int y, int x, int p) //sterowanie rakieta
 {
-        mapa[x][y] = 219;
-        mapa[x-1][y] = 30;
-        mapa[x+1][y+1] = 92;
-        mapa[x+1][y-1] = 47;
-        mapa[x+1][y] = 42;
-
-
+    switch(p)
+    {
+    case 1: //gora
+        {
+            mapa[y][x] = 219; //kadlub
+        //gora, dol statku
+            mapa[y-1][x] = 30;
+            mapa[y+1][x] = 42;
+        //boki
+            mapa[y+1][x+1] = 92;
+            mapa[y+1][x-1] = 47;
+        }break;
+    case 2: //prawo
+        {
+            mapa[y][x] = 219; //kadlub
+        //gora, dol statku
+            mapa[y][x+1] = 16;
+            mapa[y][x-1] = 42;
+        //boki
+            mapa[y-1][x-1] = 92;
+            mapa[y+1][x-1] = 47;
+        }break;
+    case 3: //dol
+        {
+            mapa[y][x] = 219; //kadlub
+        //gora, dol statku
+            mapa[y+1][x] = 31;
+            mapa[y-1][x] = 42;
+        //boki
+            mapa[y-1][x+1] = 47;
+            mapa[y-1][x-1] = 92;
+        }break;
+    case 4: //lewo
+        {
+            mapa[y][x] = 219; //kadlub
+        //gora, dol statku
+            mapa[y][x-1] = 17;
+            mapa[y][x+1] = 42;
+        //boki
+            mapa[y-1][x+1] = 47;
+            mapa[y+1][x+1] = 92;
+        }break;
+    }
+}
+void Mapa::cleaner(int y, int x, int p) //usuwa pozostalosci po rakiecie
+{
+    switch(p)
+    {
+        case 1:
+            {
+                mapa[y+2][x] = space;
+                mapa[y+2][x+1] = space;
+                mapa[y+2][x-1] = space;
+                mapa[y][x-1] = space;
+                mapa[y][x+1] = space;
+                mapa[y-1][x-1] = space;
+                mapa[y-1][x+1] = space;
+            }break;
+        case 2:
+            {
+                mapa[y-1][x-2] = space;
+                mapa[y][x-2] = space;
+                mapa[y+1][x-2] = space;
+                mapa[y+1][x] = space;
+                mapa[y-1][x] = space;
+                mapa[y+1][x+1] = space;
+                mapa[y-1][x+1] = space;
+            }break;
+        case 3:
+            {
+                mapa[y-2][x] = space;
+                mapa[y-2][x+1] = space;
+                mapa[y-2][x-1] = space;
+                mapa[y][x-1] = space;
+                mapa[y][x+1] = space;
+                mapa[y+1][x-1] = space;
+                mapa[y+1][x+1] = space;
+            }break;
+        case 4:
+            {
+                mapa[y-1][x+2] = space;
+                mapa[y][x+2] = space;
+                mapa[y+1][x+2] = space;
+                mapa[y+1][x] = space;
+                mapa[y-1][x] = space;
+                mapa[y+1][x-1] = space;
+                mapa[y-1][x-1] = space;
+            }break;
+    }
 }
